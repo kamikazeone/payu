ae2e730c8b52298b77d8aca0b9dada27fd27e72a
kamikazeone <44550472+kamikazeone@users.noreply.github.com>
2019-11-04 15:15:56 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+
+$useragent = "Mozilla/5.0 (Linux; Android 7.1.2; Redmi Note 5a) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.62 Mobile Safari/537.36";
+$token = "a1e55a188914e61b3a37aaf2de804e9e9ca1a456";
+$PHPSESSID = "PHPSESSID=n98ii1cn75aro6tv5m8uq9vkus";
+$cfduid = "__cfduid=dbb34a9a871f27c7d74c07111c843ecd01572854368";
